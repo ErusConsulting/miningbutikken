36f2ff7621b857e26570aa172f86ac90a53d63ff
Thomas Presthus <thomas@erus.net>
2018-03-09 23:54:42 +0100

    Initial commit with a vue implementation. This may change..

---
+++ src/components/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <!-- Navigation -->
+    <div class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <router-link to="/" class="navbar-item">Miningbutikken.no</router-link>
+      </div>
+      <div class="navbar-menu">
+        <div class="navbar-start">
+          <cart-summary></cart-summary>
+        </div>
+        <div class="navbar-end">
+          <a class="navbar-item" href="post@miningbutikken.no">Kontakt oss</a>
+        </div>
+      </div>
+    </div>
+
+    <router-view></router-view>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Product.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="section">
+    <div class="container">
+      <div class="columns" style="min-height: 800px">
+        <div class="column is-6">
+          <div class="image is-2by2">
+            <img src="https://cdn.scotch.io/2842/b7yhhuUPSGO1fEkMHD6P_sticks.jpeg">
+          </div>
+        </div>
+        <div class="column is-5 is-offset-1">
+          <div class="title is-2">
+            {{ productName }}
+          </div>
+          <p class="title is-3 has-text-muted">
+            kr. {{ price }},-
+          </p>
+          <hr />
+          <br />
+          <p>
+            {{ longDescription }}
+          </p>
+          <br />
+          <br />
+          <p class="level">
+            <button
+                class="snipcart-add-item button is-large is-primary ievel-item has-text-centered"
+                v-bind:data-item-id="productId"
+                v-bind:data-item-name="productName"
+                v-bind:data-item-price="price"
+                v-bind:data-item-weight="weight"
+                data-item-url="/"
+                v-bind:data-item-description="shortDescription">
+                    Kjøp nå
+            </button>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      productId: 2,
+      weight: 20,
+      productName: 'Bundle of sticks',
+      price: 15,
+      longDescription: 'This bundle of sticks provides an earthy fragrance and aesthetic natural appeal. Your bundle of sticks will come with a personalized note. The perfect natural gift for any time of year!',
+      shortDescription: 'Fresh sticks'
+    }
+  }
+}
+</script>
